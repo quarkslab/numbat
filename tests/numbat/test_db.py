ab75bb13e0a57eeb09e4addc70d7728c86c2f0e0
Eloïse Brocas <ebrocas@quarkslab.com>
2024-02-19 11:32:36 +0100

    [fix] tests: update tests after all updates of v0.2.0

--- tests/numbat/test_db.py
+++ tests/numbat/test_db.py
@@ -1,5 +1,5 @@
 from numbat import SourcetrailDB
-from numbat.base import NameHierarchy, NameElement, NodeType
+from numbat.types import NameHierarchy
 
 import pathlib
 import shutil
@@ -11,14 +11,14 @@ TMP_PATH = 'generated'
 @pytest.fixture(scope='session', autouse=True)
 def prepare_and_clean():
     """
-        This function will be called once before running all 
+        This function will be called once before running all
         the test and once after all the test have been run.
     """
-    
+
     # Create a new directory
     os.makedirs(TMP_PATH)
     yield
-    # Delete it 
+    # Delete it
     shutil.rmtree(TMP_PATH)
 
 @pytest.fixture()
@@ -26,14 +26,13 @@ def test_create_db():
     print('test_create_db')
     path = '%s/db.srctrldb' % TMP_PATH
 
-    # Clean if already exists 
+    # Clean if already exists
     if os.path.exists(path):
         os.remove(path)
 
     # Create a new database
-    srctrl = SourcetrailDB()
     try:
-        srctrl.create(path)
+        srctrl = SourcetrailDB.create(path)
         srctrl.close()
         assert True
     except Exception as e:
@@ -45,9 +44,8 @@ def test_open_database(test_create_db):
     path = '%s/db.srctrldb' % TMP_PATH
 
     # Open an existing database
-    srctrl = SourcetrailDB()
     try:
-        srctrl.open(path)
+        srctrl = SourcetrailDB.open(path)
         srctrl.clear()
         srctrl.close()
         assert True
@@ -59,9 +57,8 @@ def test_record_file(test_create_db):
     path = '%s/db.srctrldb' % TMP_PATH
 
     # Open an existing database
-    srctrl = SourcetrailDB()
-    srctrl.open(path)
-    
+    srctrl = SourcetrailDB.open(path)
+
     filename = '%s/test.c' % TMP_PATH
     with open(filename, 'w') as test:
         test.write('''
@@ -69,7 +66,7 @@ def test_record_file(test_create_db):
                 return 0;
             }
         ''')
-    
+
         file_id = srctrl.record_file(pathlib.Path(filename))
         assert file_id != None
         srctrl.record_file_language(file_id, 'C')
@@ -77,106 +74,31 @@ def test_record_file(test_create_db):
 
     srctrl.close()
 
-@pytest.fixture()
-def test_record_symbol(test_create_db):
+def test_record_class(test_create_db):
     path = '%s/db.srctrldb' % TMP_PATH
 
     # Open an existing database
-    srctrl = SourcetrailDB()
-    srctrl.open(path)
- 
-    hierarchy = NameHierarchy(
-        NameHierarchy.NAME_DELIMITER_JAVA,
-        [NameElement(
-            '',
-            'MyType',
-            ''
-        )]
-    )
+    srctrl = SourcetrailDB.open(path)
     # Insert a symbol once
-    id_a = srctrl.record_symbol(hierarchy)
+    id_a = srctrl.record_class(name='MyType', delimiter=NameHierarchy.NAME_DELIMITER_JAVA)
     assert(id_a != None)
 
-    # Check that symbol exists
-    assert(id_a == srctrl.get_symbol(hierarchy))
-    
-    srctrl.close()
-    
-def test_record_symbol_kind(test_record_symbol):
-    path = '%s/db.srctrldb' % TMP_PATH
-
-    # Open an existing database
-    srctrl = SourcetrailDB()
-    srctrl.open(path)
-
-    hierarchy = NameHierarchy(
-        NameHierarchy.NAME_DELIMITER_JAVA,
-        [NameElement(
-            '',
-            'MyType',
-            ''
-        )]
-    )
-    # Insert a symbol once
-    id_a = srctrl.record_symbol(hierarchy)
-    assert(id_a != None)
-
-    srctrl.record_symbol_kind(id_a, NodeType.NODE_CLASS)
-    assert True 
     srctrl.close()
 
-def test_record_symbol_kind(test_record_symbol):
-    path = '%s/db.srctrldb' % TMP_PATH
-
-    # Open an existing database
-    srctrl = SourcetrailDB()
-    srctrl.open(path)
-
-    hierarchy = NameHierarchy(
-        NameHierarchy.NAME_DELIMITER_JAVA,
-        [NameElement(
-            '',
-            'MyType',
-            ''
-        )]
-    )
-    # Insert a symbol once
-    id_a = srctrl.record_symbol(hierarchy)
-    assert(id_a != None)
-
-    srctrl.record_symbol_kind(id_a, NodeType.NODE_CLASS)
-    assert True 
-    srctrl.close()
-    
 def test_duplicate_symbol(test_create_db):
     path = '%s/db.srctrldb' % TMP_PATH
 
     # Open an existing database
-    srctrl = SourcetrailDB()
-    srctrl.open(path)
- 
+    srctrl = SourcetrailDB.open(path)
+
     # Insert a symbol once
-    id_a = srctrl.record_symbol(NameHierarchy(
-        NameHierarchy.NAME_DELIMITER_JAVA,
-        [NameElement(
-            '',
-            'MyType',
-            ''
-        )]
-    ))
+    id_a = srctrl.record_class(name='MyType', delimiter=NameHierarchy.NAME_DELIMITER_JAVA)
     assert(id_a != None)
-    
+
     # Insert the same symbol again
-    id_b = srctrl.record_symbol(NameHierarchy(
-        NameHierarchy.NAME_DELIMITER_JAVA,
-        [NameElement(
-            '',
-            'MyType',
-            ''
-        )]
-    ))
+    id_b = srctrl.record_class(name='MyType', delimiter=NameHierarchy.NAME_DELIMITER_JAVA)
+
     assert(id_b != None)
     assert(id_a == id_b)
 
     srctrl.close()
-
